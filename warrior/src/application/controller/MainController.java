466449fb70294019fb6fcf2c5bafa82a5bb07f9a
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-04 09:48:54 -0500

    commit

--- warrior/src/application/controller/MainController.java
+++ warrior/src/application/controller/MainController.java
@@ -12,32 +12,35 @@ import javafx.scene.Scene;
 
 public class MainController implements EventHandler<ActionEvent> {
 	
-	@Override
+	
 	public void handle(ActionEvent event) {
-		System.out.println("1");
 		// print a message to the console (debugging step, to ensure everything is connected)
 		try {
 			// change over to a second view
 			Parent root = FXMLLoader.load(getClass().getResource("/SecondView.fxml"));   // Load the FXML
 			Main.stage.setScene(new Scene(root, 500, 575));							   // Add the scene to the stage
-			Main.stage.show();														   // Show the stage to the user
+			Main.stage.show();		
+			// Show the stage to the user
 		}catch(Exception e) {
 			e.printStackTrace(); // TODO: app should do something more productive if errors occur...
 		}
-		
 	}
+		public void handleOptions(ActionEvent event) {
+			try {
+				// change over to a second view
+				Parent root = FXMLLoader.load(getClass().getResource("/OptionsView.fxml"));   // Load the FXML
+				Main.stage.setScene(new Scene(root, 500, 575));							   // Add the scene to the stage
+				Main.stage.show();														   // Show the stage to the user
+			}catch(Exception e) {
+				e.printStackTrace(); // TODO: app should do something more productive if errors occur...
+			}
+		}
+	
 	public void handleCloseButton(ActionEvent event) {
 		Platform.exit();
 	}
-	public void handleOptions(ActionEvent event) {
-		System.out.println("2");
-		try {
-			// change over to a second view
-			Parent root = FXMLLoader.load(getClass().getResource("/OptionsView.fxml"));   // Load the FXML
-			Main.stage.setScene(new Scene(root, 500, 575));							   // Add the scene to the stage
-			Main.stage.show();														   // Show the stage to the user
-		}catch(Exception e) {
-			e.printStackTrace(); // TODO: app should do something more productive if errors occur...
-		}
+	
+		
 	}
-}
+	
+
