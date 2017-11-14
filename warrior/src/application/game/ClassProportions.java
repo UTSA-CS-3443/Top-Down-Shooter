4139be463a75db68a9bb907f06b8f7ff795346c3
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-14 13:30:52 -0800

    window pane size and size of the warrior

---
+++ warrior/src/application/game/ClassProportions.java
@@ -0,0 +1,11 @@
+package application.game;
+
+public class ClassProportions {
+	
+	public static final double WINDOW_W = 1200;
+	public static final double WINDOW_H = 800;
+	
+	public static final double WARRIOR_W = 25;
+	public static final double WARRIOR_H = 25;
+
+}
