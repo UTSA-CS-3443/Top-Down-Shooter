b10ea83f4724c7ca98ac2c802c2b63eb5a725710
JeromeD123 <jerome.daly26@gmail.com>
2017-11-14 16:23:06 -0600

    Updating the mannaquin class

--- warrior/src/application/game/Mannequin.java
+++ warrior/src/application/game/Mannequin.java
@@ -1,9 +1,26 @@
 package application.game;
 
+
 public class Mannequin {
-	int health;
+	int health = Health.ENEMY_HEALTH;
 	int X_VALUE;
 	int Y_VALUE;
 	boolean IS_TOUCHING = false;
 	
+	public void setPosition(int x, int y) {
+		this.X_VALUE = x;
+		this.Y_VALUE = y;
+	}
+	public int getX_VALUE() {
+		return this.X_VALUE;
+	}
+	public int getY_VALUE() {
+		return this.Y_VALUE;
+	}
+	public void setTouch(boolean x) {
+		this.IS_TOUCHING = x;
+	}
+	public boolean getTouch() {
+		return this.IS_TOUCHING;
+	}
 }
