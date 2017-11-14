76342e651447d2acc8c308cd931fe62b8a8512b7
JeromeD123 <jerome.daly26@gmail.com>
2017-11-14 15:39:03 -0600

    added a health class to track the player health and the enemy health

---
+++ warrior/src/application/game/Health.java
@@ -0,0 +1,11 @@
+package application.game;
+
+public class Health {
+	//player health
+	public static final int PLAYER_HEALTH = 100;
+	public static final int PLAYER_HEALTH_LOSS = 10;
+	
+	//enemy health
+	public static final int ENEMY_HEALTH = 100;
+	public static final int ENEMY_HEALTH_LOSS = 33;
+}
