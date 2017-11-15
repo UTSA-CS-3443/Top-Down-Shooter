033b0b1db3e59c1f3ae0aa8260e6ec6f660f3254
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-15 13:44:28 -0600

    JCole

--- warrior/src/application/game/Timing.java
+++ warrior/src/application/game/Timing.java
@@ -1,5 +1,13 @@
 package application.game;
 
 public class Timing {
-
+	
+	public static final double TICK_LENGTH =5;
+	
+	public static final double PLAYER_DELTA_X = 2.0;
+	public static final double PLAYER_DELTA_Y = 2.0;
+	
+	public static final double MANNEQUIN_X = 2.5;
+	
+	
 }
