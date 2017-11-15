9a98ad32238c241374e2e193f7c941f16a531ee4
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-15 11:27:24 -0800

    making the splash class - usher

--- warrior/src/application/game/Splash.java
+++ warrior/src/application/game/Splash.java
@@ -10,6 +10,17 @@ public class Splash extends Thing {
 		super();
 		this.damage = damage;
 	}
-
-
+	public Splash(String name, double W, double H, int damage) {
+		super();
+		this.damage = damage;
+	}
+	public Splash(String name, double W, double H, int damage, double startX, double startY, double dX, double dY) {
+		super();
+		this.damage = damage;
+		this.dX = dX;
+		this.dY = dY;
+		setX(startX);
+		setY(startY);
+	}
+	
 }
