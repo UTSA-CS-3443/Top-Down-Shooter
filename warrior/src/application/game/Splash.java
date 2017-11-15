07a6dd825907fa4267e0ec8ef6bb58b6948c609f
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-15 11:06:32 -0800

    splash jv

--- warrior/src/application/game/Splash.java
+++ warrior/src/application/game/Splash.java
@@ -1,5 +1,14 @@
 package application.game;
 
-public class Splash {
+public class Splash extends Thing {
+	
+	protected double dX = 0.0;
+	protected double dY = 0.0;
+	protected int damage = 1000;
+	
+	public Splash(String name, int damage) {
+		super();
+		this.damage = damage;
+	}
 
 }
