58d544048bc1c909cf9e618c700d6578bcea6fcd
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-16 12:56:19 -0600

    T-Pain

---
+++ warrior/src/application/game/Resources.java
@@ -0,0 +1,11 @@
+package application.game;
+
+public class Resources {
+	public static final String BASE = "media/";
+	
+	public static final String SPR_WARRIOR = BASE + "mervin.png";
+	public static final String SPR_WARRIOR_SPLASH = BASE + "";
+	
+	public static final String SPR_MANNEQUIN = BASE + "MervinManikin";
+	public static final String SPR_MANNEQUIN_SPLASH = BASE + "";
+}
--- warrior/src/application/game/WarriorSplash.java
+++ warrior/src/application/game/WarriorSplash.java
@@ -1,15 +1,17 @@
 package application.game;
 
+import application.ClassProportions;
+
 public class WarriorSplash extends Splash {
 
 	public WarriorSplash() {
-		super(Resource.SPRITE_FOR_SPLASH_WARRIOR, ClassProportions.SPLASH_WARRIOR_W
-				,ClassProportions.SPLASH_H, Healh. SPLASH_WARRIOR_DMG);
+		super(Resources.SPR_WARRIOR_SPLASH, ClassProportions.SPLASH_WARRIOR_W
+				,ClassProportions.SPLASH_WARRIOR_H, Health.SPLASH_WARRIOR_DMG);
 		// TODO Auto-generated constructor stub
 	}
 	public WarriorSplash(double originX, double originY)
 	{
-		super(Resources.SPRITE_FOR_SPLASH_WARRIOR, ClassProportions.SPLASH_WARRIOR_W,
+		super(Resources.SPR_WARRIOR_SPLASH, ClassProportions.SPLASH_WARRIOR_W,
 				ClassProportions.SPLASH_WARRIOR_H, Health.SPLASH_WARRIOR_DMG,
 				originX,originY,Timing.SPLASH_WARRIOR_DELTA_X,Timing.SPLASH_WARRIOR_DELTA_Y);
 	}
