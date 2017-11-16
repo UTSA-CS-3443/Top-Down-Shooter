8b9e65828dbb57f0aaebe693f7d9b0282ffeded6
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-16 12:41:57 -0600

    joji

--- warrior/src/application/game/WarriorSplash.java
+++ warrior/src/application/game/WarriorSplash.java
@@ -7,5 +7,10 @@ public class WarriorSplash extends Splash {
 				,ClassProportions.SPLASH_H, Healh. SPLASH_WARRIOR_DMG);
 		// TODO Auto-generated constructor stub
 	}
-	
+	public WarriorSplash(double originX, double originY)
+	{
+		super(Resources.SPRITE_FOR_SPLASH_WARRIOR, ClassProportions.SPLASH_WARRIOR_W,
+				ClassProportions.SPLASH_WARRIOR_H, Health.SPLASH_WARRIOR_DMG,
+				originX,originY,Timing.SPLASH_WARRIOR_DELTA_X,Timing.SPLASH_WARRIOR_DELTA_Y);
+	}
 }
