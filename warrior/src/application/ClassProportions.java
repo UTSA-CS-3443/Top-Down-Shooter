6d8138895c1c0ee0e8df906176a445121daa41b3
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-16 12:43:52 -0600

    Lofi

--- warrior/src/application/ClassProportions.java
+++ warrior/src/application/ClassProportions.java
@@ -2,16 +2,16 @@ package application;
 
 public class ClassProportions {
 	
-	public static final double WINDOW_W = 1200;
-	public static final double WINDOW_H = 800;
+	public static final double WINDOW_W = 1200.0;
+	public static final double WINDOW_H = 800.0;
 	
-	public static final double WARRIOR_W = 25;
-	public static final double WARRIOR_H = 25;
-    public static final double MANNEQUIN_W = 32;
-    public static final double MANNEQUIN_H= 32;
-    public static final double SPLASH_WARRIOR_W = 8;
-    public static final double SPLASH_WARRIOR_H = 8;
-    public static final double SPLASH_MANNEQUIN_W = 6;
-    public static final double SPLASH_MANNEQUIN_H= 6;
+	public static final double WARRIOR_W = 25.0;
+	public static final double WARRIOR_H = 25.0;
+    public static final double MANNEQUIN_W = 32.0;
+    public static final double MANNEQUIN_H= 32.0;
+    public static final double SPLASH_WARRIOR_W = 8.0;
+    public static final double SPLASH_WARRIOR_H = 8.0;
+    public static final double SPLASH_MANNEQUIN_W = 6.0;
+    public static final double SPLASH_MANNEQUIN_H= 6.0;
 
 }
