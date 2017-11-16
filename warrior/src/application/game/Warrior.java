0ca0e896485e69df1af8a1d9bf713c47b7000cc7
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-16 12:28:54 -0800

    more warrior updating - jv

--- warrior/src/application/game/Warrior.java
+++ warrior/src/application/game/Warrior.java
@@ -20,6 +20,11 @@ public class Warrior extends Mob {
 	protected double deltaX = Timing.WARRIOR_DELTA_X;
 	protected double deltaY = Timing.WARRIOR_DELTA_Y;
 	
+	private static Image leftSprite = new Image(Resources.SPR_WARRIOR, 
+			ClassProportions.WARRIOR_W, ClassProportions.WARRIOR_H, false, false);
+    private static Image rightSprite = new Image(Resources.SPR_WARRIOR, 
+    		ClassProportions.WARRIOR_W, ClassProportions.WARRIOR_H, false, false);
+	
 	public Warrior() {
 		super(Resources.SPR_WARRIOR, ClassProportions.WARRIOR_W,
 				ClassProportions.WARRIOR_H, Health.WARRIOR_HEALTH);
