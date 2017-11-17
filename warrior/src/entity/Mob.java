d88723e1c3b5b1b5b8886dad97938906667d076d
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-17 11:07:05 -0800

    updated mob class - jv

--- warrior/src/entity/Mob.java
+++ warrior/src/entity/Mob.java
@@ -1,7 +1,9 @@
 package entity;
 
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ReadOnlyIntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+
 
 public abstract class Mob extends Entity {
     private int health;
@@ -9,9 +11,36 @@ public abstract class Mob extends Entity {
 	
 	public Mob() {}
 	
-	public Mob(int Health) {
+	public Mob(int health) {
 		this.health = health;
-		healthProperty.set(maxHealth);
+		healthProperty.set(health);
 	}
-	
+	public Mob(String spriteName, int health) {
+		super(spriteName);
+		this.health = health;
+		healthProperty.set(health);
+	}
+	public Mob(String spriteName, double width, double height, int health) {
+        super(spriteName, width, height);
+        this.health = health;
+        healthProperty.set(health);
+    }
+	public int getHealth() {
+		return health;
+	}
+	public int getProperty() {
+		return healthProperty.get();
+	}
+	public void setProperty(int health) {
+		healthProperty.set(health);
+	}
+	public void subtractHealth(int health) {
+		healthProperty.set(healthProperty.get() - health);
+	}
+	public void addHealth(int health) {
+		healthProperty.set(healthProperty.get() - health);
+	}
+	public ReadOnlyIntegerProperty healthPropertyUnmodifiable() {
+        return healthProperty;
+    }
 }
