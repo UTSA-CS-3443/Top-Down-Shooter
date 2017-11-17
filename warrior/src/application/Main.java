4c4d438333b4a67b4cd91de3907f74e9aeacccac
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-17 13:16:16 -0600

    nothing

--- warrior/src/application/Main.java
+++ warrior/src/application/Main.java
@@ -1,6 +1,10 @@
 package application;
 
 
+
+
+
+import entity.Warrior;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -22,6 +26,9 @@ public class Main extends Application {
 			//all of my stuff
 			primaryStage.setTitle("Paint Warrrior");
 			
+
+			Warrior warrior = new Warrior();   //BVP 1329 11/7
+
 			
 			
 			root.setId("pane");
@@ -34,44 +41,22 @@ public class Main extends Application {
 			    if(e.getCode() == KeyCode.D){
 			        newX = newX + 8;
 			        circle.setTranslateX(newX);
-			        System.out.println("Right");
 			    }
 			    else if(e.getCode() == KeyCode.A){
 			        newX = newX - 8;
 			        circle.setTranslateX(newX);
-			        System.out.println("Left");
 			    }
 			    else if(e.getCode() == KeyCode.W){
 			        newY = newY - 8;
 			        circle.setTranslateY(newY);
-			        System.out.println("Up");
 			    }
 			    else if(e.getCode() == KeyCode.S){
 			        newY = newY + 8;
 			        circle.setTranslateY(newY);
-			        System.out.println("Down");
 			    }
+			    else SPACE:
+			    	warrior.fireProjectile();
 			});
-			/*int x = warrior.getPaintCount();
-			scene.setOnKeyPressed(e->{
-				if(e.getCode() == KeyCode.Q) {
-					warrior.paintCount++;
-					if(warrior.paintCount == 4)
-						warrior.paintCount = 0;
-					switch(warrior.paintCount) {
-					case 0:
-						//warrior.paintColor = Color.RED;
-						System.out.println("case: 0");
-					case 1:
-						//warrior.paintColor = Color.BLUE;
-						System.out.println("case: 1");
-					case 2:
-						//warrior.paintColor = Color.GREEN;
-					}
-				}
-			});*/
-			
-			//end of my stuff
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- warrior/src/entity/Warrior.java
+++ warrior/src/entity/Warrior.java
@@ -1,45 +1,115 @@
+
 package entity;
 
-import application.Health;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
-
-
-
+import application.Timing;
+import application.game.*;
+import application.Figurations;
+import application.Health;
+import application.WarriorProjectiles;
 
 public class Warrior extends Mob {
-	
-	
-	
-			
-			// Should be in this format to create a Player - BVP 13:11 11/17
-			/*
-			public static Image image1 = new Image(String url, double requestedWidth, double requestedHeight,
-					boolean preserveRatio,boolean smooth);*/
+    
+    private static Image leftSprite = new Image(Resources.WARRIOR, 
+            Figurations.WARRIOR_WIDTH, Figurations.WARRIOR_HEIGHT, false, false);
+    
+    //The amount to move each tick
+    protected double deltaX = Timing.WARRIOR_DELTA_X;
+    protected double deltaY = Timing.WARRIOR_DELTA_Y;
+    
+    protected boolean movingLeft = false;
+    protected boolean movingRight = false;
+    protected boolean movingUp = false;
+    protected boolean movingDown = false;
+    
 
-	
-	private int health = Health.WARRIOR_HEALTH;
-	private int paintCount = 0;
-	
-	public int getHealth() {
-		return this.health;
-	}
-	public void takeDamage() {
-		this.health = this.health - Health.ENEMY_DMG;
-	}
-	public int getPaintCount() {
-		return this.paintCount;
-	}
-	
-	
-	
-		//	
-/*
-			root.setId("pane");
-			Circle circle = new Circle(50, 20, 20, Color.RED);
-			circle.setCenterX(400);
-			circle.setCenterY(300);
-			root.getChildren().add(circle);*/
+    public Warrior() {
+        super(Resources.WARRIOR, Figurations.WARRIOR_WIDTH, 
+                Figurations.WARRIOR_HEIGHT, Health.WARRIOR_HEALTH);
+        setInitialPosition();
+    }
+    
 
+    private void setInitialPosition() {
+        setX((Figurations.SCREEN_WIDTH - Figurations.WARRIOR_WIDTH) / 2);
+        setY(Figurations.SCREEN_HEIGHT - Figurations.WARRIOR_HEIGHT);
+    }
+    
+ 
+    public void startMovement(Direction dir) {
+        switch (dir) {
+            case UP:
+                movingUp = true; break;
+            case DOWN:
+                movingDown = true; break;
+            case LEFT:
+                movingLeft = true; break;
+            case RIGHT:
+                movingRight = true; break;
+        }
+    }
+    
+
+    public void stopMovement(Direction dir) {
+        switch (dir) {
+            case UP:
+                movingUp = false; break;
+            case DOWN:
+                movingDown = false; break;
+            case LEFT:
+                movingLeft = false; break;
+            case RIGHT:
+                movingRight = false; break;
+        }
+    }
+
+    public void fireProjectile() {
+        ((GamePane)getParent()).getEngine().queueAddition(
+            new WarriorProjectiles(
+                getCenterX() - (Figurations.BULLET_WIDTH / 2),
+                getY() - Figurations.BULLET_HEIGHT
+            )
+        );
+    }
+    
+    protected void doMovement() {
+        if (movingLeft ^ movingRight) { 
+            if (movingLeft) {
+                if (getX() >= deltaX) setX(getX() - deltaX);
+                else setX(0);
+            }
+            if (movingRight) {
+                if (getX() < (Figurations.SCREEN_WIDTH - width - deltaX))
+                    setX(getX() + deltaX);
+                else setX(Figurations.SCREEN_WIDTH - width - deltaX);
+            }
+        }
+        if (movingUp ^ movingDown) {
+            if (movingUp) {
+                if (getY() >= (deltaY + height)) setY(getY() - deltaY);
+                else setY(height);
+            }
+            if (movingDown) {
+                //Prevent player from leaving the game area at the bottom
+                if (getY() < (Figurations.SCREEN_HEIGHT - height - deltaY))
+                    setY(getY() + deltaY);
+                else setY(Figurations.SCREEN_HEIGHT - height - deltaY);
+            }
+        }
+    }
+    
+    /**
+     * Processes events for this Entity.
+     * PreCondition: None.
+     * PostConditon: This Player has moved and had a sprite set according to 
+     * the direction in which it moved.
+     */
+    @Override
+    public void doTick() {
+        doMovement();
+        //Set the sprite according to how we're moving
+        if (movingLeft ^ movingRight) {
+            if (movingLeft) setImage(leftSprite);
+        } else setImage(sprite);
+    }
 }
