1fd36eae8c78ca17b7446bea7f4caf138db1e6cc
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-17 12:18:38 -0600

    does this do anything

--- warrior/src/application/Projectiles.java
+++ warrior/src/application/Projectiles.java
@@ -1,17 +1,81 @@
+
 package application;
+import entity.Entity;
+import application.game.*;
+public class Projectiles extends Entity {
 
-import com.sun.prism.paint.Color;
+   
+    protected double deltaX = 0.0;
+    protected double deltaY = 0.0;
+    protected int damage = 9999;
+    
 
-import javafx.scene.shape.Circle;
+    public Projectiles() {
+        super(Resources.BULLET, 8, 8);
+    }
+    
 
-public class Projectiles extends Entity{
-	protected double deltaX = 0.0;
-	protected double deltaY = 0.0;
-	protected int damage = 9999;
-	
-	public Projectile()
-	{
-		Circle circle = new Circle(2,2,2);
-		
-	}
+    public Projectiles(String spriteName, int damage) {
+        super(spriteName, 8, 8);
+        this.damage = damage;
+    }
+    
+ 
+    public Projectiles(String spriteName, double width, double height, int damage) {
+        super(spriteName, width, height);
+        this.damage = damage;
+    }
+    
+  
+    public Projectiles(String spriteName, double width, double height, int damage,
+            double originX, double originY, double deltaX, double deltaY) {
+        super(spriteName, width, height);
+        this.damage = damage;
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+        setX(originX);
+        setY(originY);
+    }
+
+ 
+    public double getDeltaX() {
+        return deltaX;
+    }
+
+
+    public void setDeltaX(double deltaX) {
+        this.deltaX = deltaX;
+    }
+
+ 
+    public double getDeltaY() {
+        return deltaY;
+    }
+
+
+    public void setDeltaY(double deltaY) {
+        this.deltaY = deltaY;
+    }
+
+
+    public int getDamage() {
+        return damage;
+    }
+
+
+    public void setDamage(int damage) {
+        this.damage = damage;
+    }
+
+    protected void doMovement() {
+        setX(getX() - deltaX);
+        setY(getY() - deltaY);
+    }
+    
+
+    @Override
+    public void doTick() {
+        doMovement();
+    }
+    
 }
---
+++ warrior/src/application/game/Resources.java
@@ -0,0 +1,5 @@
+package application.game;
+
+public class Resources {
+	public static final String BULLET = "media/bullet4.png";
+}
