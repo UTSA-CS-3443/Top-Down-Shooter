b95abb20f9cd843d0000a96a98385d0f1692efcf
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-20 11:55:19 -0800

    updating main - jv

--- warrior/src/application/Main.java
+++ warrior/src/application/Main.java
@@ -3,11 +3,12 @@ package application;
 import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
-//import javafx.scene.Parent;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 //import javafx.scene.layout.AnchorPane;
 //import javafx.scene.layout.BorderPane;
 //import application.Resources;
@@ -15,18 +16,28 @@ import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
-	
+	private static final int SCREEN_WIDTH = 640;
+	private static final int SCREEN_HEIGHT = 640;
 	//making it simple for the getScene and this scene functions
 	Stage window;
+	private Pane root = new Pane();
 	GameStarter engine = new GameStarter();
 	Scene sceneGame = engine.getScene();
-	Scene mainMenu;
+	Scene menu;
 	
-	public static void main(String[] args)
-    {
-    	launch(args);
-    }
 	
+	
+	private Parent mainMenu() {
+		addBackground();
+		return root;
+	}
+	private void addBackground() {
+		ImageView imageView = new ImageView(new Image(getClass().getResource("/media/bobross.png").toExternalForm()));
+        imageView.setFitWidth(SCREEN_WIDTH);
+        imageView.setFitHeight(SCREEN_HEIGHT);
+
+        root.getChildren().add(imageView);
+	}
 	@Override
 	public void start(Stage primaryStage) throws Exception{
 		window = primaryStage;
@@ -34,16 +45,20 @@ public class Main extends Application {
 		Button buttonNG = new Button("NEW GAME");
 		buttonNG.setOnAction(e -> window.setScene(sceneGame));
         
-		VBox layout = new VBox(20);
+		VBox layout = new VBox(100);
 		//we need to add more buttons pertaining this function
 		layout.getChildren().addAll(buttonNG);
-		mainMenu = new Scene(layout, 640, 640);
-	
-		window.setScene(mainMenu);
-		window.setTitle("PAINT WARRIOR");
+		menu = new Scene(layout, 640, 640);
 		
+		//Scene scene = new Scene(mainMenu());
+		window.setTitle("PAINT WARRIOR");
+		window.setScene(menu);
         window.show();
 	}
+	public static void main(String[] args)
+    {
+    	launch(args);
+    }
     
     
 }
