bd44c2ef18bb3ddaa4379118ffa82f138a1f7e80
Joshua Villarreal <villarrealjoshua29@gmail.com>
2017-11-21 10:43:36 -0600

    i didnt do anything to this

--- warrior/src/application/Main.java
+++ warrior/src/application/Main.java
@@ -6,7 +6,6 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
