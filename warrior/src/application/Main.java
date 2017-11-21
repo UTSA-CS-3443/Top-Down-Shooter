48f9e7b52abb20f533a24dda3d1ff058c2ae70a1
Jheremi Villarreal <jheremi2015@gmail.com>
2017-11-21 12:17:31 -0800

    fixing the main menu still needs updating -jv

--- warrior/src/application/Main.java
+++ warrior/src/application/Main.java
@@ -1,62 +1,90 @@
 package application;
 	
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
 //import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-<<<<<<< HEAD
+import javafx.scene.effect.SepiaTone;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-=======
->>>>>>> branch 'master' of https://github.com/UTSA-CS-3443/Top-Down-Shooter.git
+
 //import javafx.scene.layout.AnchorPane;
 //import javafx.scene.layout.BorderPane;
 //import application.Resources;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
 
 
 public class Main extends Application {
 	private static final int SCREEN_WIDTH = 640;
 	private static final int SCREEN_HEIGHT = 640;
-	//making it simple for the getScene and this scene functions
-	Stage window;
-	private Pane root = new Pane();
+	
+//	private Pane root = new Pane();
+	//to start the game
 	GameStarter engine = new GameStarter();
 	Scene sceneGame = engine.getScene();
-	Scene menu;
+	//making it simple for the getScene and this scene functions
+	Stage window = new Stage();
+	
+//	Scene menu;
 	
 	
 	
 	private Parent mainMenu() {
-		addBackground();
-		return root;
-	}
-	private void addBackground() {
-		ImageView imageView = new ImageView(new Image(getClass().getResource("/media/bobross.png").toExternalForm()));
-        imageView.setFitWidth(SCREEN_WIDTH);
+		Pane root = new Pane();
+		ImageView imageView = new ImageView(new Image(getClass()
+                .getResource("/media/bobross.png")
+                .toExternalForm()));
+		
+		imageView.setFitWidth(SCREEN_WIDTH);
         imageView.setFitHeight(SCREEN_HEIGHT);
 
-        root.getChildren().add(imageView);
+        SepiaTone tone = new SepiaTone(0.85);
+        imageView.setEffect(tone);
+
+        Rectangle masker = new Rectangle(SCREEN_WIDTH, SCREEN_HEIGHT);
+        masker.setOpacity(0);
+        masker.setMouseTransparent(true);
+        
+        MenuBox menuBox = new MenuBox(250, 350);
+        menuBox.setTranslateX(250);
+        menuBox.setTranslateY(230);
+        
+        MenuItem itemNew = new MenuItem("NEW GAME", 250);
+        itemNew.setOnAction(() -> {
+        	window.setScene(sceneGame);
+        	window.show();
+        });
+        menuBox.addItem(itemNew);
+        
+        MenuItem itemExit = new MenuItem("EXIT", 250);
+        itemExit.setOnAction(() -> System.exit(0));
+        menuBox.addItem(itemExit);
+        
+        root.getChildren().addAll(imageView, menuBox, masker);
+        return root;
 	}
 	@Override
 	public void start(Stage primaryStage) throws Exception{
-		window = primaryStage;
+		//window = primaryStage;
        // GameStarter engine = new GameStarter();
-		Button buttonNG = new Button("NEW GAME");
-		buttonNG.setOnAction(e -> window.setScene(sceneGame));
+//		Button buttonNG = new Button("NEW GAME");
+//		buttonNG.setOnAction(e -> window.setScene(sceneGame));
         
-		VBox layout = new VBox(100);
+//		VBox layout = new VBox(100);
 		//we need to add more buttons pertaining this function
-		layout.getChildren().addAll(buttonNG);
-		menu = new Scene(layout, 640, 640);
+//		layout.getChildren().addAll(buttonNG);
+		//menu = new Scene(layout, 640, 640);
 		
-		//Scene scene = new Scene(mainMenu());
-		window.setTitle("PAINT WARRIOR");
-		window.setScene(menu);
-        window.show();
+		Scene sceneGame = new Scene(mainMenu());
+		primaryStage.setTitle("PAINT WARRIOR");
+		primaryStage.setScene(sceneGame);
+		primaryStage.show();
 	}
 	public static void main(String[] args)
     {
